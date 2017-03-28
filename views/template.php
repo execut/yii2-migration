6b66e88dfeec17662dc1621829d46d554804beda
eXeCUT <sexecut@gmail.com>
2017-03-28 15:37:36 +0300

    added template for migration generator

---
+++ views/template.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This view is used by console/controllers/MigrateController.php
+ * The following variables are available in this view:
+ */
+/* @var $className string the new migration class name without namespace */
+/* @var $namespace string the new migration class namespace */
+
+echo "<?php\n";
+if (!empty($namespace)) {
+    echo "\nnamespace {$namespace};\n";
+}
+?>
+
+use execut\yii\migration\Migration;
+use execut\yii\migration\Inverter;
+
+class <?= $className ?> extends Migration
+{
+    public function initInverter(Inverter $i)
+    {
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
